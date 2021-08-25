22d320132a86e23f71b1e0eff18d81c1939ec821
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-08-25 11:06:12 -0600

    frame resize issue

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -475,13 +475,13 @@ frame1 = Frame(root)
 frame1.grid(row=0, column=0, sticky="nsew")
 
 
-button_takePicture = Button(frame1, text="Take Picture", bg="#fdad5c", height=4, width=10, command=take_photo)#, command=lambda: take_picture(raw_filename))
-button_viewPicture = Button(frame1, text="View Image", bg="#fdad5c", height=4,  width=10,command=openImage)
-button_createSpectrum = Button(frame1, text="Create Spectrum", bg="#fdad5c", height=4,width=10, command=createSpectrum) #, command=createSpectrum)
-button_viewSpectrum = Button(frame1, text="View Spectrum", bg="#fdad5c", height=4,width=10, command=openSpectrum)
+button_takePicture = Button(frame1, text="Take Picture", bg="#fdad5c", height=4, command=take_photo)#, command=lambda: take_picture(raw_filename))
+button_viewPicture = Button(frame1, text="View Image", bg="#fdad5c", height=4,  command=openImage)
+button_createSpectrum = Button(frame1, text="Create Spectrum", bg="#fdad5c", height=4, command=createSpectrum) #, command=createSpectrum)
+button_viewSpectrum = Button(frame1, text="View Spectrum", bg="#fdad5c", height=4, command=openSpectrum)
 
 
-exit_button = Button(frame1, text="Exit",height=1,width=10, command=root.destroy)
+exit_button = Button(frame1, text="Exit",height=1, command=root.destroy)
 exit_button.grid(row=4,column=0)
 
 # New windows
