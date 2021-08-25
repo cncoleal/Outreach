c66514d099f3c36fb070e5add51d10d7574ab675
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-08-25 14:56:04 -0600

    frame resize issue

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -20,7 +20,7 @@ from tkinter import filedialog
 root = Tk()
 # root.withdraw()
 root.title('Spectrometer')
-root.geometry("600x600+140")
+root.geometry("600x600+140+0")
 #root.attributes("-fullscreen", 1)+
 root.configure(bg="black")
 frame = Frame(root, bg="blue")
