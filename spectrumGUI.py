047474671bb1f8b43ca0c39f1912ec3567c4edc1
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-09 12:48:47 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -463,6 +463,11 @@ def openSpectrum():
 
 def pic_capture():
     cap = cv2.VideoCapture(0)
+    cv2.namedWindow("Video Capture", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
+    #m = cv2.imread("earth.jpg")  # Read image
+    #imS = cv2.resize(im, (960, 540))  # Resize image
+    #cv2.imshow("output", imS)
+
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
@@ -476,8 +481,8 @@ def pic_capture():
         # Our operations on the frame come here
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # Display the resulting frame
-        cv2.imshow('frame', img)
-        cv2.resize('frame', wid-wid_but, hgt-50)
+        cv2.imshow('Video Capture', cv2.resize(img, (wid-wid_but, hgt-50)))
+        #cv2.resize('', wid-wid_but, hgt-50)
         #cv2.moveWindow('image', 0, 0) #(wid-wid_but, hgt-50, wid_but, 0) (lxw+delx+dely)
         if cv2.waitKey(1) == ord('q'):
             break
