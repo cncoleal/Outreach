b1eb76e158b8e71ba7747834db0cc92a739ca7ba
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-09 12:46:16 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -477,7 +477,8 @@ def pic_capture():
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # Display the resulting frame
         cv2.imshow('frame', img)
-        cv2.moveWindow('image', 0, 0) #(wid-wid_but, hgt-50, wid_but, 0) (lxw+delx+dely)
+        cv2.resize('frame', wid-wid_but, hgt-50)
+        #cv2.moveWindow('image', 0, 0) #(wid-wid_but, hgt-50, wid_but, 0) (lxw+delx+dely)
         if cv2.waitKey(1) == ord('q'):
             break
     # When everything done, release the capture
