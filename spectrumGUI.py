349192e52550c854dd6e1ef214ebe14fc9cf5909
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-13 15:21:29 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -226,7 +226,7 @@ def take_picture(name, shutter):
         camera.framerate_range = (0.167, 6)  # this should match the values available in sensor mode, allowing upto a 6 second exposure
         camera.exposure_mode = 'nightpreview'
         #camera.framerate = Fraction(1, 3)
-        # camera.shutter_speed = shutter
+        camera.shutter_speed = 5
         # camera.iso = 1000
         # camera.exposure_mode = 'off'
         # camera.awb_mode = 'off'
