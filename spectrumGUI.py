89808fc040faf98c0d4e89ddc4fc99ab6f7bb2cb
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-13 14:42:13 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -549,7 +549,7 @@ def openVideo():
     shutter = int(5)
     #camera = picamera.PiCamera()
     camera.start_preview(fullscreen=False, window=(wid_but, 20, 800-wid_but-17, 500))
-    camera.resolution (2592,1944)
+    camera.resolution = (2592,1944)
     camera.brightness = 50
     camera.sensor_mode = 3
     camera.iso = 0  # Auto.This will yield less noise during day exposures and keep the iso down in low light for less noise.
