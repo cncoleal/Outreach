9979bdd2ff8b171b2b29458a1f2bcf03272bc2b8
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-13 10:11:39 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -535,7 +535,7 @@ def openVideo():
     wid_but = 140
 
     camera = picamera.PiCamera()
-    camera.start_preview(fullscreen=False, window=(140, 0, 800, 480))
+    camera.start_preview(fullscreen=False, window=(140, 0, 800, 100))
 
 
 # openVideo Function that Works
