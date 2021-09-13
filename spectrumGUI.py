b824a969d957bd8e7df6fbd0663c335c2fdf2c1a
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-13 12:25:51 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -542,7 +542,7 @@ def openVideo():
 
     shutter = int(5)
     #camera = picamera.PiCamera()
-    camera.start_preview(fullscreen=False, window=(wid_but, 30, 800-wid_but-20, 470))
+    camera.start_preview(fullscreen=False, window=(wid_but, 33, 800-wid_but-17, 470))
     camera.vflip = True
     #camera.framerate = Fraction(1, 3)
     camera.shutter_speed = shutter
