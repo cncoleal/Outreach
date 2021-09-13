c3cc6e72c66f2a592da98b03fcd6d0bb5d5440d6
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-13 14:22:38 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -545,7 +545,7 @@ def openVideo():
     camera.start_preview(fullscreen=False, window=(wid_but, 20, 800-wid_but-17, 500))
     camera.resolution (2592,1944)
     camera.sensor_mode = 3
-    camera.iso = 0  # Auto.This will yield less noise during day exposures and keep the iso down in low light for less noise.
+    camera.iso = 300  # Auto.This will yield less noise during day exposures and keep the iso down in low light for less noise.
     camera.framerate_range = (0.167, 6)  # this should match the values available in sensor mode, allowing upto a 6 second exposure
     camera.exposure_mode = 'nightpreview'  # raises the gains, and lowers the iso
     #sleep = (10)
