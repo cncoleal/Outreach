1b38d31e7f1f3ca2c56201d62d4248fdcb388a2c
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-13 15:27:06 -0600

    adding in video capture

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -37,7 +37,7 @@ root.title('Spectrometer')
 root.geometry('%dx%d+%d+%d' % (wid-wid_but, hgt+30, wid_but, -30))
 root.configure(bg="black")
 frame = Frame(root, bg="blue")
-frame.grid(row=0, column=0, sticky="nsew", anchor='s')
+frame.grid(row=0, column=0, sticky="s")
 
 # side window: displays buttons
 butWin = tk.Toplevel(root) # Tk()
