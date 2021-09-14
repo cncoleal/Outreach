212bd0d9fcd929d1c30dd356963e20a248b0ba05
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-09-14 16:45:04 -0600

    swapping brightness for shutter adjustment

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -422,7 +422,7 @@ def openImage():
 def openSpectrum():
     camera.stop_preview()
     w = root.winfo_width()
-    h = root.winfo_height()-40
+    h = root.winfo_height()-35
 
     ## To open spectrum
     simg = PIL.Image.open(output_chart)
