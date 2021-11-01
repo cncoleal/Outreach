4f961d0bae3aed395facf4eb645af6ca77cfac4a
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-11-01 13:23:47 -0600

    november updates

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -328,6 +328,7 @@ def export_diagram(normalized_results):
         lambda2 = 1.0 / (f1 - (float(x) / float(w) * (f1 - f2)))
         c = wavelength_to_color(lambda2)
         draw.line((x, 0, x, h), fill=c)
+        draw.line((x,h,x,h), fill='#000', width=15)
 
     pl = [(w, 0), (w, h)]
     for wavelength in normalized_results:
