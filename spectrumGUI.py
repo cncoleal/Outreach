403b4a83e63e43aff23b0491bca4361da2658721
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-11-01 16:15:30 -0600

    november updates

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -348,10 +348,14 @@ def export_diagram(normalized_results,aperture, spectrum_angle):
         if count == 0:
             prev_point_x = int(x)
             prev_point_y = int((1 - normalized_results[wavelength]) * h)
-        else:
+        elif count % 5 == 0:
             draw.line((int(x), int((1 - normalized_results[wavelength]) * h), prev_point_x, prev_point_y), fill="#000", width=spectrum_line_width)
 
             # store previous points
+            prev_point_x = int(x)
+            prev_point_y = int((1 - normalized_results[wavelength]) * h)
+
+        else:
             prev_point_x = int(x)
             prev_point_y = int((1 - normalized_results[wavelength]) * h)
 
