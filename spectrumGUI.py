ec56d25bcdfd98f2be42c4281191dc19a6791a48
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-11-01 16:30:08 -0600

    november updates

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -346,65 +346,15 @@ def export_diagram(normalized_results,aperture, spectrum_angle):
         pl.append((int(x), int((1 - normalized_results[wavelength]) * h))) # ordered dictionary
         line_points.append((int(x), int((1 - normalized_results[wavelength]) * h)))
 
-        # draw a line connecting the two points
-        # if count == 0:
-        #     prev_point_x = int(x)
-        #     prev_point_y = int((1 - normalized_results[wavelength]) * h)
-        # elif count % 3 == 0: # draw line between every 5 points
-        #
-        #     draw.line((int(x), int((1 - normalized_results[wavelength]) * h), prev_point_x, prev_point_y), fill="#000", width=spectrum_line_width)
-        #
-        #     # store previous points
-        #     prev_point_x = int(x)
-        #     prev_point_y = int((1 - normalized_results[wavelength]) * h)
 
-
-
-        # else:
-        #     prev_point_x = int(x)
-        #     prev_point_y = int((1 - normalized_results[wavelength]) * h)
-
-        #count = count+1
-        #print(normalized_results[wavelength])
-        # add to pl list -- (x_position associated with wavelength,
     pl.append((0, h))
     pl.append((0, 0))
 
-    #print(pl)
     draw.polygon(pl, fill="#fff")  # background color
     draw.polygon(pl)
 
-   # line_points = [(100, 100), (150, 200), (300, 100), (500, 300)]
+
     draw.line(line_points, width=40, fill='#000', joint='curve')
-
-
-    #draw_scan_line(aperture,draw, spectrum_angle )
-
-    # del_line_wgt = 60 #controls thickness of black outline on spectrum
-    # plw = [(w, 0+del_line_wgt), (w, h+del_line_wgt)] #pl = [(w, 0), (w, h)]
-    # plb = [(w,0), (w,h)] # black line
-    # for wavelength in normalized_results:
-    #     wl = float(wavelength)
-    #     x = int((wl - w1) / (w2 - w1) * w)
-    #     # print wavelength,x
-    #     plw.append((int(x), int((1 - normalized_results[wavelength]) * h)+del_line_wgt))
-    #     plb.append((int(x), int((1-normalized_results[wavelength])*h)))
-    # plw.append((0, h+del_line_wgt))
-    # plw.append((0, 0+del_line_wgt))
-    #
-    # plb.append((0, h))
-    # plb.append((0,0))
-    #
-    # draw.polygon(plw, fill="#000")
-    # draw.polygon(plw)
-    #
-    # draw.polygon(plb, fill="#fff") # background color
-    # draw.polygon(plb)
-
-
-
-
-
 
     font = PIL.ImageFont.truetype('/usr/share/fonts/truetype/lato/Lato-Regular.ttf', 12 * antialias)
     draw.line((0, h, w, h), fill="#000", width=antialias) # bottom solid line on spectrum
