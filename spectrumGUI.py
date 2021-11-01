d14d222ab5dfdf1bdcb8a86e1be27c174980324f
cncoleal <44885944+cncoleal@users.noreply.github.com>
2021-11-01 15:48:28 -0600

    november updates

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -353,16 +353,16 @@ def export_diagram(normalized_results,aperture, spectrum_angle):
     #draw_scan_line(aperture,draw, spectrum_angle )
 
 
-    plw = [(w, 0+5), (w, h+5)] #pl = [(w, 0), (w, h)]
+    plw = [(w, 0+15), (w, h+15)] #pl = [(w, 0), (w, h)]
     plb = [(w,0), (w,h)] # black line
     for wavelength in normalized_results:
         wl = float(wavelength)
         x = int((wl - w1) / (w2 - w1) * w)
         # print wavelength,x
-        plw.append((int(x), int((1 - normalized_results[wavelength]) * h)))
+        plw.append((int(x), int((1 - normalized_results[wavelength]) * (h+15))))
         plb.append((int(x), int((1-normalized_results[wavelength])*h)))
-    plw.append((0, h+5))
-    plw.append((0, 0+5))
+    plw.append((0, h+15))
+    plw.append((0, 0+15))
 
     plb.append((0, h))
     plb.append((0,0))
