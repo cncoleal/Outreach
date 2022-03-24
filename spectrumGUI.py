9ab80efe430871761ad3d11c68180e593591fe54
cncoleal <44885944+cncoleal@users.noreply.github.com>
2022-03-24 09:42:59 -0600

    March Updates, exit pop-up

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -51,7 +51,7 @@ Win1.grid(row=0, column=0, sticky="nsew")
 
 def open_popup():
    top= Toplevel(butWin)
-   top.geometry('750x500') #%dx%d+%d+%d' % (wid/2, hgt/2, wid/4, hgt/4))
+   top.geometry('%dx%d+%d+%d' % (wid/2, hgt/2, wid/4, hgt/4))
    top.title("Exit")
    Label(top, text="Shutdown Now?", font='Mistral 18 bold').place(x=wid/4, y=hgt/4)
 
