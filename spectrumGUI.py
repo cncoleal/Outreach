15540b09098133b7311b09ab8505a44e812e9820
cncoleal <44885944+cncoleal@users.noreply.github.com>
2022-03-24 11:16:17 -0600

    March Updates, exit pop-up

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -60,12 +60,12 @@ def open_popup():
     top.geometry('%dx%d+%d+%d' % (wid / 2, hgt / 2, wid / 4, hgt / 4))
     Label(top, text="Shutdown Now?", font='Mistral 18 bold').pack(side=TOP, pady=10)#place(x=wid/8, y=20)
 
-    shutdown_button_yes = Button(top, text="Yes", font='Mistral 18 bold', bg="#AAFF00", height=5, command=root.destroy) #will need to change this
+    shutdown_button_yes = Button(top, text="Yes", font='Mistral 16 bold', bg="#AAFF00", height=5, command=root.destroy) #will need to change this
     shutdown_button_yes.config(width=10, height=5, activebackground="#568156", relief=RAISED, justify='left')
     shutdown_button_yes.pack(side=LEFT, padx=10)
     #shutdown_button_yes.grid(row=2,column=0)
 
-    shutdown_button_no = Button(top, text="No",font='Mistral 18 bold', bg="#bf0000", fg='white', height=5, command=top.destroy)
+    shutdown_button_no = Button(top, text="No",font='Mistral 16 bold', bg="#bf0000", height=5, command=top.destroy)
     shutdown_button_no.config(width=10, height=5, activebackground="#f26161", relief=RAISED, justify='center')
     #shutdown_button_no.place(x=wid/4+15, y=60)
     shutdown_button_no.pack(side=LEFT, padx=10)
