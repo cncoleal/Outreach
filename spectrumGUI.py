6d037b96f01dce343f9d8482ebfd475d2548d8a3
cncoleal <44885944+cncoleal@users.noreply.github.com>
2022-03-25 08:40:45 -0600

    March updates, loading window for picture taking

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -701,7 +701,7 @@ button_s10p.config(activebackground="#568156",relief=RAISED)
 
 #button_takePicture = Button(butWin, text="Take Picture", bg="#fdad5c", height=5, command=lambda: [loading_popup()])
 #, button_start(), button_main(), button_end(), loadWin.destroy()  loading_popup(), pause(5), loadWin.place_forget(), loadWin.destroy()
-button_takePicture = Button(butWin, text="Take Picture", bg="#fdad5c", height=5, command=lambda: [loading_popup(), button_start(), button_main(), button_end()])
+button_takePicture = Button(butWin, text="Take Picture", bg="#fdad5c", height=5, command=lambda: [loading_popup, button_start, button_main, button_end])
 button_takePicture.config(activebackground="#ffdbb7")
 button_viewPicture = Button(butWin, text="View Image",  bd=0, bg="#fdad5c", height=5,  command=openImage)
 button_viewPicture.config(activebackground="#ffdbb7")
