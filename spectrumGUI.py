c692b8ebedd2a4af51d712a7a442c116b2b78a36
cncoleal <44885944+cncoleal@users.noreply.github.com>
2022-03-25 11:00:16 -0600

    March updates, loading window for picture taking

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -50,15 +50,15 @@ Win1 = Frame(butWin)
 Win1.grid(row=0, column=0, sticky="nsew")
 
 # create frame for the loading message
-loadWin = tk.Toplevel(root)
-loadWin.geometry('%dx%d+%d+%d' % (wid / 2, hgt / 8, wid / 4, hgt / 4))
-loadWin.configure(bg="white")
-Win3 = Frame(loadWin)
-Win3.place(relx=0.5, rely=0.5, anchor=CENTER)
-    #geometry('%dx%d+%d+%d' % (wid / 2, hgt / 2, wid / 4, hgt / 4))
-#loadWin = Frame(root)
+#loadWin = tk.Toplevel(root)
+#loadWin.geometry('%dx%d+%d+%d' % (wid / 2, hgt / 8, wid / 4, hgt / 4))
 #loadWin.configure(bg="white")
-#loadWin.place(relx=0.5, rely=0.5, anchor=CENTER)
+#Win3 = Frame(loadWin)
+#Win3.place(relx=0.5, rely=0.5, anchor=CENTER)
+    #geometry('%dx%d+%d+%d' % (wid / 2, hgt / 2, wid / 4, hgt / 4))
+loadWin = Frame(root)
+#loadWin.configure(bg="white")
+loadWin.place(relx=0.5, rely=0.5, anchor=CENTER)
 #grid(row=0, column=0, sticky="nsew")
 #root.geometry('%dx%d+%d+%d' % (wid-wid_but-wid_slide, hgt, wid_but+wid_slide+1,-30))
 #root.configure(bg="black")
@@ -86,9 +86,9 @@ def open_popup():
 
 def loading_popup():
     #Label(loadWin, text="Please Wait. Taking Picture.", font='Mistral 18 bold').pack(side=TOP, pady=10)
-    top = tk.Toplevel(root)
-    top.geometry('%dx%d+%d+%d' % (wid / 2, hgt / 4, wid / 4, hgt / 4))
-    Label(top, text="Please Wait. Taking Picture.", font='Mistral 18 bold', bg="#FFFFFF").pack(side=BOTTOM, pady=10, padx=10)  # place(x=wid/8, y=20)
+    #top = tk.Toplevel(root)
+    #top.geometry('%dx%d+%d+%d' % (wid / 2, hgt / 4, wid / 4, hgt / 4))
+    Label(loadWin, text="Please Wait. Taking Picture.", font='Mistral 18 bold', bg="#FFFFFF").pack(side=BOTTOM, pady=10, padx=10)  # place(x=wid/8, y=20)
     return
 
 
