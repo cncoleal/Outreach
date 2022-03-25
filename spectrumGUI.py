20b04168931e3180e5e55b7b5b71bd7eb0da103b
cncoleal <44885944+cncoleal@users.noreply.github.com>
2022-03-25 09:10:43 -0600

    March updates, loading window for picture taking

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -323,7 +323,7 @@ def take_picture(imname, shutter):
     camera.exposure_mode = 'off'
     camera.awb_mode = 'off'
     camera.awb_gains = (1, 1)
-    #root.after(1000, pseudo_sleep())
+    root.after(1000, pseudo_sleep())
     #camera.capture(imname, resize=(wid - wid_but, hgt))
     return
 
