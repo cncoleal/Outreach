364b5601a4a7c1914a5b1905fa7768088a31a493
cncoleal <44885944+cncoleal@users.noreply.github.com>
2022-03-28 08:41:45 -0600

    March updates, "Image Acquired" Popup

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -707,7 +707,7 @@ button_viewPicture.config(activebackground="#ffdbb7")
 #button_createSpectrum = Button(butWin, text="Create Spectrum", bg="#fdad5c", height=4, command=createSpectrum)
 button_viewSpectrum = Button(butWin, text="View Spectrum", bd=0, bg="#fdad5c", height=5, command=openSpectrum)
 button_viewSpectrum.config(activebackground="#ffdbb7")
-button_captureVideo = Button(butWin, text="Video Capture", bg="#fdad5c",  height=5, command=root.destroy)  #openVideo)
+button_captureVideo = Button(butWin, text="Video Capture", bg="#fdad5c",  height=5, command=openVideo)
 button_captureVideo.config(activebackground="#ffdbb7")
 
 
