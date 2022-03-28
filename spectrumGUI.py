189d107b8e28887bb323457229af663e156650da
cncoleal <44885944+cncoleal@users.noreply.github.com>
2022-03-28 08:04:09 -0600

    March updates, "Image Acquired" Popup

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -720,9 +720,9 @@ def lambda_for_picture():
     button_start()
     button_main() # this button causes the pause
     button_end()
-    loading_popup()
+    #loading_popup()
     #root.after(1000, pseudo_sleep())
-    loadWin.destroy()
+    #loadWin.destroy()
 
    # loadWin.place_forget()
    # loadWin.destroy()
